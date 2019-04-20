6051016552e4e8c03d6f1d9961ca5ed438edf772
EC2 Default User <ec2-user@ip-172-31-35-145.us-east-2.compute.internal>
2019-04-20 11:34:15 +0000

    Finish user signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
-  before_save { self.email = email.downcase! }
+  #before_save { self.email = email.downcase! }
+  #before_save self.email = email.downcase
+  before_save { email.downcase! if email }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
   validates :email, presence: true, length: { maximum: 255}, 
