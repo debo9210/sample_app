6552e0938f925df8f7f95dd8e0fd335b81e8c7f2
EC2 Default User <ec2-user@ip-172-31-35-145.us-east-2.compute.internal>
2019-04-30 09:45:28 +0000

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
   #before_save { self.email = email.downcase! }
   #before_save self.email = email.downcase
-  attr_accessor :remember_token
-  before_save { email.downcase! if email }
+  attr_accessor :remember_token, :activation_token
+  before_save   :downcase_email                #{ email.downcase! if email }
+  before_create :create_activation_digest
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/
   validates :email, presence: true, length: { maximum: 255}, 
@@ -36,9 +37,38 @@ class User < ApplicationRecord
     BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
   
+   def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
+   end
+   
+     # Activates an account.
+  def activate
+    update_attribute(:activated,    true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+  
+    def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+    end
+  
    # Forgets a user.
   def forget
     update_attribute(:remember_digest, nil)
   end
+  
+  private
+  
+   def downcase_email
+     #self.email = email.downcase
+     email.downcase!
+     #email.downcase! if email
+   end
+   
+   def create_activation_digest
+     self.activation_token  = User.new_token
+     self.activation_digest = User.digest(activation_token)
+   end
 
 end
